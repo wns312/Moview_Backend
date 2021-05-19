77c926d47443017d6ff8a697970afb6c61893278
김준영 <wns312@gmail.com>
2021-05-20 02:02:29 +0900

    User모델 주석 추가

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -3,17 +3,13 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     # 필드 재정의
-    email = models.EmailField(
-      max_length=150,
-      help_text = 'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
-      error_messages = {
-        'unique': "A user with that username already exists.",
-      },
-      unique=True
-    )
+    email = models.EmailField(max_length=150, unique=True)
     # 유저이름 필드를 email로 변경하겠다
     USERNAME_FIELD = 'email'
-    # 유저이름 필드가 email이므로, 필요 필드에 username과 password를 적는 것
+    # createsuperuser 커맨드로 유저를 생성할 때 나타날 필드 이름 목록
+    # USERNAME_FIELD로 지정된 필드는 REQUIRED_FIELDS에서 제외되어야 한다고 한다.
+    # 따라서 email은 REQUIRED_FIELDS에서 제외되어야 한다. (자동으로 나온다)
+    # username은 포함되어야 한다.
     REQUIRED_FIELDS = ('username', 'password')
 
     def __str__(self):
