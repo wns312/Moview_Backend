1a0af03c6d8e62758f747b24e2a7e10d26b03199
wns312 <wns312@gmail.com>
2021-05-20 17:15:31 +0900

    비밀번호 확인도 유효성 검사 비통과시  형식 맞춰줌

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,9 @@ def signup(request):
   # 1. 비밀번호와 비밀번호 확인이 일치하는지 확인한다.
   password = request.data.get('password')
   password_confirm = request.data.get('passwordConfirm')
-  if password != password_confirm : return Response({""}, status=status.HTTP_400_BAD_REQUEST) 
+  if password != password_confirm : 
+    # 이렇게 쓴 이유는 isvalid에서 튕겨져 나오는 형식을 맞춰주기 위함임
+    return Response({"passwordConfirm" : {"비밀번호 확인이 일치하지 않습니다"}}, status=status.HTTP_400_BAD_REQUEST) 
 
   # 2. serializer로 받아서 저장을 한다. 그리고 결과로 serializer의 data를 리턴한다.
   serializer = UserSerializer(data=request.data)
