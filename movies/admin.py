46090d4880d3a648c393bc9d6ae04190a93c6409
김준영 <wns312@gmail.com>
2021-05-25 18:02:02 +0900

    관리자페이지 설정 완료

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie, Genre, Prefer
 
-# Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    filter_horizontal = ('movie',)
+admin.site.register(Movie)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Prefer)
