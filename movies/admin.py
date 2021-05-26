ee59c3cba60998b51e2bb6f6b5ec5f4b36b3b80e
김준영 <wns312@gmail.com>
2021-05-26 09:17:23 +0900

    Genre Admin

--- movies/admin.py
+++ movies/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Movie, Genre, Prefer
 
 class GenreAdmin(admin.ModelAdmin):
-    filter_horizontal = ('movie',)
+    filter_horizontal = ('movies',)
+
 admin.site.register(Movie)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Prefer)
